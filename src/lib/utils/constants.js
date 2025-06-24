3dc8ba2927fef25af94dc7845a442600a7b7183f
fgitonga <mfredgitonga@gmail.com>
2025-06-24 12:32:53 +0300

    feat: add constants for TMDB image URLs and placeholder images

---
+++ src/lib/utils/constants.js
@@ -0,0 +1,7 @@
+// TMDB
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const TMDB_POSTER_SIZE_W500 = 'w500';
+
+// Placeholders
+export const PLACEHOLDER_MOVIE_POSTER = '/images/placeholder-movie.jpg';
+export const PLACEHOLDER_TV_POSTER = '/images/placeholder-tv.jpg'; 
